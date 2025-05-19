62165f48253529cef207ee403173b8aaee07b054
wwpl <>
2025-05-19 23:02:56 +0000

    dbがなくても起動するモードを追加

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppNoDbModule } from './app-no-db.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const moduleToLoad =
+    process.env.APP_MODULE === 'noDb' ? AppNoDbModule : AppModule;
+  const app = await NestFactory.create(moduleToLoad);
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
