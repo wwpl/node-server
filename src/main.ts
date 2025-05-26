d665d555ea98bd005ba7702321ddb8258151b086
wwpl <>
2025-05-26 23:03:23 +0000

    マルチコア化

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as os from 'os';
+import * as _cluster from 'cluster';
+const cluster = _cluster as unknown as _cluster.Cluster; // typings fix
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3000);
+  if (cluster.isMaster) {
+    // ここを isMaster → isPrimary に変更
+    console.log(`Master process ${process.pid} is running`);
+
+    const numCPUs = os.cpus().length;
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`Worker ${worker.process.pid} died, restarting...`);
+      cluster.fork();
+    });
+  } else {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+    console.log(`Worker process ${process.pid} started on port 3000`);
+  }
 }
+
 bootstrap();
