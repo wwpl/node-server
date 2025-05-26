5bd4c7c79a2cac988bcbd6efa882a750b9120b0b
wwpl <>
2025-05-26 23:09:34 +0000

    apiのprefixを追加

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
     });
   } else {
     const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
     await app.listen(3000);
     console.log(`Worker process ${process.pid} started on port 3000`);
   }
